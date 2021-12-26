5696ea60d8a61b7542a3a7447df78716a58e25ab
KaiZen42 <katsalai@protonmail.com>
2021-12-26 23:31:10 +0100

    upd

--- src/back/src/app.controller.ts
+++ src/back/src/app.controller.ts
@@ -1,12 +1,22 @@
+import { PrismaService } from './prisma/prisma.service';
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+	  private readonly appService: AppService,
+	  private readonly prismaService: PrismaService,) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('user')
+  getUser(): Promise<User[]> {
+	return this.prismaService.user.findMany();
+	  
   }
 }

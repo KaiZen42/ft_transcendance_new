d367338fb788301d29c57c7a6d8379d981d17169
KaiZen42 <katsalai@protonmail.com>
2022-01-27 00:42:55 +0100

    FRONT-4 upd

--- src/front/src/pages/Dashboard.tsx
+++ src/front/src/pages/Dashboard.tsx
@@ -2,10 +2,12 @@ import axios from "axios";
 import React, { Component, FormEvent, useEffect, useRef, useState} from "react";
 import Wrapper from "../components/Wrapper";
 import { User } from "../models/User.interface";
+import {Navigate} from "react-router-dom";
 
 export default function Dashboard() {
   const [user, setUser] = useState<User>();
-  const [selectedFile, setSelectedFile] = useState<File>();
+	const [selectedFile, setSelectedFile] = useState<File>();
+	const [image, setImage] = useState(false)
 
   useEffect(() => {(
     async () => {
@@ -15,29 +17,45 @@ export default function Dashboard() {
   )();
   }, []);
 
-  const submitForm = () => {
-    const formData = new FormData();
-    formData.append("to_upload", selectedFile!);
+//   const submitForm = () => {
+//     const formData = new FormData();
+//     formData.append("to_upload", selectedFile!);
   
-    axios
-      .post("http://localhost:3000/api/users/image", formData, {withCredentials: true})
-      .then(() => {
-        alert("File Upload success");
-      })
-      .catch(() => alert("File Upload Error"));
-  };
-
-  const handleFileInput = (e : React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedFile(e.target.files![0])
-  }
-
+//     axios
+//       .post("http://localhost:3000/api/users/image", formData, {withCredentials: true})
+//       .then(() => {
+//         alert("File Upload success");
+//       })
+//       .catch(() => alert("File Upload Error"));
+//   };
+	const upload = async (files: FileList | null) => {
+		if (files === null) return;
+		const formData = new FormData();
+		formData.append("to_upload", files[0]);
+		await axios
+			.post("http://localhost:3000/api/users/image", formData, { withCredentials: true })
+		
+		setImage(true)		
+	}
+		
+	if (image) {
+		window.location.reload();
+	}
+  
+//   const handleFileInput = (e : React.ChangeEvent<HTMLInputElement>) => {
+//     setSelectedFile(e.target.files![0])
+//   }
+  
   return (
     <Wrapper>
       <div>
-        <img src={user?.avatar}/>
-        <form onSubmit={submitForm}>
-          <input type="file" onChange={handleFileInput} />
-          <input type="submit" />
+        <img src={user?.avatar} width="250px"/>
+			  {/* <form onSubmit={submitForm}> */}
+			  <form>
+		<label className="btn btn-primary">
+					  Upload image<input type="file" hidden onChange={e => upload(e.target.files)} />
+          {/* <input type="submit" /> */}
+		</label>
         </form>
       </div>
     </Wrapper>

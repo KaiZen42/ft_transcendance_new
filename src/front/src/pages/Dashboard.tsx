c158ea6d70d6245966ac12f4929f4bc84fe8d7b1
buonve <buonvino.vincenzo@gmail.com>
2022-02-12 19:03:18 +0100

    background video

--- src/front/src/pages/Dashboard.tsx
+++ src/front/src/pages/Dashboard.tsx
@@ -1,25 +1,34 @@
 import axios from "axios";
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import Wrapper from "../components/Wrapper";
 import { User } from "../models/User.interface";
+import "../styles/video.css";
 
 export default function Dashboard() {
-  	const [user, setUser] = useState<User>();
-	
-	useEffect(() => {(
-	async () => {
-		const {data} = await axios.get(`http://${process.env.REACT_APP_BASE_IP}:3001/api/user`, {withCredentials: true});
-		setUser(data);
-	}
-	)();
-	}, []);
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get(
+        `http://${process.env.REACT_APP_BASE_IP}:3001/api/user`,
+        { withCredentials: true }
+      );
+      setUser(data);
+    })();
+  }, []);
 
   return (
     <Wrapper>
-		<NavLink to={'/game'}>
-            <button>PLAY PONG</button>
-        </NavLink>
+      <NavLink to={"/game"}>
+        <div>
+          <button>PLAY PONG</button>
+          <video autoPlay muted loop className="video">
+            <source src="movie2.mov" type="video/mov" />
+          </video>
+		  <img src="OL.png" alt="image_diocaro" className="overlay_back"/>
+        </div>
+      </NavLink>
     </Wrapper>
   );
 }

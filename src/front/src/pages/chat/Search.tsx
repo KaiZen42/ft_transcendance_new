7c2b0c9e1285c6256b5f349a4632c58f923c3ab0
KaiZen42 <katsalai@protonmail.com>
2022-02-16 19:59:50 +0100

    Bage added

--- src/front/src/pages/chat/Search.tsx
+++ src/front/src/pages/chat/Search.tsx
@@ -3,13 +3,45 @@ import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Autocomplete from '@mui/material/Autocomplete';
 import { User } from '../../models/User.interface';
+import { styled } from '@mui/material/styles';
+import Badge from '@mui/material/Badge';
 import { KeyboardReturnOutlined } from '@mui/icons-material';
+import { Avatar, Box } from '@mui/material';
 
-interface props{
-  setSearch: (str:string) => void,
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: 'ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}));
+
+interface props {
+  setSearch: (str: string) => void;
 }
 
-export default function SearchBox({setSearch}: props) {
+export default function SearchBox({ setSearch }: props) {
   const [data, setData] = useState<User[]>([]);
 
   async function getter() {
@@ -38,11 +70,25 @@ export default function SearchBox({setSearch}: props) {
 
   return (
     <>
-      <Stack spacing={2} sx={{ width: '100%', text: 'white' }}>
+      <Stack spacing={2} sx={{ width: '100%'}}>
         <Autocomplete
           id="Search a User"
           freeSolo
-          options={data.map((mappedtype) => mappedtype.username)}
+          options={data}
+          getOptionLabel={(option) => option.username}
+          renderOption={(props, option) => (
+            <Box component="li" {...props}>
+              <StyledBadge
+                overlap="circular"
+                // invisible={true}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                variant="dot"
+              >
+                <Avatar alt="Image" src={option.avatar} />
+              </StyledBadge>
+              {option.username}
+            </Box>
+          )}
           renderInput={(params) => (
             <TextField
               onKeyDown={(e) => {

fe34e5197fa43f161eb56f8ee8f4e6c002d538ab
Mattia Giordano <mgiordan@e4r13p3.42roma.it>
2022-03-16 19:23:21 +0100

    added missing servicies

--- src/back/src/chat/service/message.service.ts
+++ src/back/src/chat/service/message.service.ts
@@ -1,88 +1,82 @@
 import {
-	BadRequestException,
-	Injectable,
-	NotFoundException,
-	Query
-  } from '@nestjs/common';
-  import { InjectRepository } from '@nestjs/typeorm';
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  Query,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/models/user.entity';
-  import { getConnection, Repository } from 'typeorm';
+import { getConnection, Repository } from 'typeorm';
 import { Message } from '../models/message.entity';
-  
-  @Injectable()
-  export class MessageService {
-	constructor(
-	  @InjectRepository(Message) private readonly messageDB: Repository<Message>,
-	) {}
-  
-	async getAll(): Promise<Message[]> {
-	  return this.messageDB.find();
-	}
-  
-	async getByUser(userId: number): Promise<Message[]> {
-		return this.messageDB.find({ where: { userId } });
-	}
 
+@Injectable()
+export class MessageService {
+  constructor(
+    @InjectRepository(Message) private readonly messageDB: Repository<Message>,
+  ) {}
 
-  
-	async getByChannel(channelId: number): Promise<Message[]> {
-		let res: Message[] = await this.messageDB
-			.createQueryBuilder("message")
-			.leftJoinAndSelect("message.userId", "user")
-			.innerJoinAndSelect("message.channelId", "channel")
-			.where("message.channelId = :idd", {idd: channelId})
-			.select(["user.id", "user.username", "message"])
-			.orderBy("message.id", "ASC")
-			.getMany();
-
-		
-		/* .createQueryBuilder("message")
-		.leftJoinAndSelect("message.userId", "user")
-		.where("message.channelId = :id", {id: channelId})
-		.select(["message.id","message.userId","message.userName","message.data"])
-		.getRawMany() */
-	  return res;
-	}
-
-	async getCounterByChannel(channelId: number): Promise<number> {
-		let res: number = await this.messageDB
-			.createQueryBuilder("message")
-			.leftJoinAndSelect("message.userId", "user")
-			.innerJoinAndSelect("message.channelId", "channel")
-			.where("message.channelId = :idd", {idd: channelId})
-			.select(["user.id", "user.username", "message"])
-			.getCount();
-
-		
-		/* .createQueryBuilder("message")
-		.leftJoinAndSelect("message.userId", "user")
-		.where("message.channelId = :id", {id: channelId})
-		.select(["message.id","message.userId","message.userName","message.data"])
-		.getRawMany() */
-	  return res;
-	}
-  
-	async create(message: Message): Promise<Message> {
-	  return this.messageDB.save({
-		userId: message.userId,
-		channelId: message.channelId,
-		data: message.data,
-		sendDate: message.sendDate,
-	  });
-	}
-
-	async delete(id: number) {
-	  return this.messageDB.delete({ id });
-	}
-
-	async deleteByChan(idC: number) {
-		await this.messageDB
-			.createQueryBuilder("message")
-			.delete()
-			.from(Message)
-			.where("channelId = :idC", {idC })
-			.execute();
-	  }
-	
+  async getAll(): Promise<Message[]> {
+    return this.messageDB.find();
   }
-  
+
+  async getByUser(userId: number): Promise<Message[]> {
+    return this.messageDB.find({ where: { userId } });
+  }
+
+  async getByChannel(channelId: number): Promise<Message[]> {
+    let res: Message[] = await this.messageDB
+      .createQueryBuilder('message')
+      .leftJoinAndSelect('message.userId', 'user')
+      .innerJoinAndSelect('message.channelId', 'channel')
+      .where('message.channelId = :idd', { idd: channelId })
+      .select(['user.id', 'user.username', 'message'])
+      .orderBy('message.id', 'ASC')
+      .getMany();
+
+    /* .createQueryBuilder("message")
+		.leftJoinAndSelect("message.userId", "user")
+		.where("message.channelId = :id", {id: channelId})
+		.select(["message.id","message.userId","message.userName","message.data"])
+		.getRawMany() */
+    return res;
+  }
+
+  async getCounterByChannel(channelId: number): Promise<number> {
+    let res: number = await this.messageDB
+      .createQueryBuilder('message')
+      .leftJoinAndSelect('message.userId', 'user')
+      .innerJoinAndSelect('message.channelId', 'channel')
+      .where('message.channelId = :idd', { idd: channelId })
+      .select(['user.id', 'user.username', 'message'])
+      .getCount();
+
+    /* .createQueryBuilder("message")
+		.leftJoinAndSelect("message.userId", "user")
+		.where("message.channelId = :id", {id: channelId})
+		.select(["message.id","message.userId","message.userName","message.data"])
+		.getRawMany() */
+    return res;
+  }
+
+  async create(message: Message): Promise<Message> {
+    return this.messageDB.save({
+      userId: message.userId,
+      channelId: message.channelId,
+      data: message.data,
+      sendDate: message.sendDate,
+    });
+  }
+
+  async delete(id: number) {
+    return this.messageDB.delete({ id });
+  }
+
+  async deleteByChan(idC: number) {
+    await this.messageDB
+      .createQueryBuilder('message')
+      .delete()
+      .from(Message)
+      .where('channelId = :idC', { idC })
+      .execute();
+  }
+}
--- src/back/src/chat/service/partecipant.service.ts
+++ src/back/src/chat/service/partecipant.service.ts
@@ -1,98 +1,113 @@
 import {
-	BadRequestException,
-	Injectable,
-	NotFoundException,
-	Query
-  } from '@nestjs/common';
-  import { InjectRepository } from '@nestjs/typeorm';
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  Query,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/models/user.entity';
-  import { EntityRepository, getConnection, getRepository, Repository } from 'typeorm';
+import {
+  EntityRepository,
+  getConnection,
+  getRepository,
+  Repository,
+} from 'typeorm';
 import { Channel } from '../models/channel.entity';
 import { Partecipant } from '../models/partecipant.entity';
-  
-  @Injectable()
-  export class PartecipantService {
-	constructor(
-	  @InjectRepository(Partecipant) private readonly partecipantDB: Repository<Partecipant>,
-	) {}
-  
-	async getAll(): Promise<Partecipant[]> {
-	  return this.partecipantDB.find();
-	}
-  
-	async getByUser(userId: number): Promise<Partecipant[]> {
-	  return this.partecipantDB.find({ where: { userId } });
-	}
 
-	async getUsersIdByChan(channelId: number) : Promise<any[]>
-	{
-		//const res = await this.partecipantDB.find({ select:["userId"] ,where: {channelId}})
-		const res: any[] = await getRepository(User)
-		.createQueryBuilder("users")
-		.leftJoin(Partecipant, "partecipant", "partecipant.userId = users.id")
-		.where("partecipant.channelId = :chId", {chId: channelId})
-		.select("users.id")
-		.getMany();
-		
-		return res.map(r => r.id);
-	}
+@Injectable()
+export class PartecipantService {
+  constructor(
+    @InjectRepository(Partecipant)
+    private readonly partecipantDB: Repository<Partecipant>,
+  ) {}
 
-	
-	async getByChannel(channelId: number): Promise<Partecipant[]> {
-	  return this.partecipantDB.find({ where: { channelId } });
-	}
+  async getAll(): Promise<Partecipant[]> {
+    return this.partecipantDB.find();
+  }
 
-	async getCompletePartecipantByChannel(channelId: number): Promise<any[]> {
-		const res: any[] = await this.partecipantDB
-		.createQueryBuilder("partecipant")
-		.select(["partecipant", "users"])
-		.leftJoinAndSelect("partecipant.userId", "users", "users.id = partecipant.userId")
-		.where("partecipant.channelId = :chId AND partecipant.mod != 'b'", {chId: channelId})
-		.getMany();
-		console.log("ret:",res)
-		return res;
-	  }
+  async getByUser(userId: number): Promise<Partecipant[]> {
+    return this.partecipantDB.find({ where: { userId } });
+  }
 
-	async isPartecipant(channelId: number, userId: number): Promise<boolean> {
-		const ret = await this.partecipantDB.findOne({ where: { channelId, userId } });
-		return (ret !== undefined )
-	}
+  async getUsersIdByChan(channelId: number): Promise<any[]> {
+    //const res = await this.partecipantDB.find({ select:["userId"] ,where: {channelId}})
+    const res: any[] = await getRepository(User)
+      .createQueryBuilder('users')
+      .leftJoin(Partecipant, 'partecipant', 'partecipant.userId = users.id')
+      .where('partecipant.channelId = :chId', { chId: channelId })
+      .select('users.id')
+      .getMany();
 
-	async getPartecipantByUserAndChan( userId: number, channelId: number): Promise<Partecipant> {
-		const ret = await this.partecipantDB.findOne({ where: { channelId, userId } });
-		return ret;
-	}
-  
-	async create(partecipant: Partecipant): Promise<Partecipant> {
-	  return this.partecipantDB.save({
-		userId: partecipant.userId,
-		channelId: partecipant.channelId,
-		muted : partecipant.muted,
-		mod : partecipant.mod,
-	  });
-	}
+    return res.map((r) => r.id);
+  }
 
-	async delete(id: number) {
-	  return this.partecipantDB.delete({ id });
-	}
+  async getByChannel(channelId: number): Promise<Partecipant[]> {
+    return this.partecipantDB.find({ where: { channelId } });
+  }
 
-	async deleteAllByChan(chanId: number)
-	{
-		return await this.partecipantDB
-		.createQueryBuilder()
-		.delete()
-		.from(Partecipant)
-		.where("channelId = :chId", { chId: chanId })
-		.execute();
-	}
+  async getCompletePartecipantByChannel(channelId: number): Promise<any[]> {
+    const res: any[] = await this.partecipantDB
+      .createQueryBuilder('partecipant')
+      .select(['partecipant', 'users'])
+      .leftJoinAndSelect(
+        'partecipant.userId',
+        'users',
+        'users.id = partecipant.userId',
+      )
+      .where("partecipant.channelId = :chId AND partecipant.mod != 'b'", {
+        chId: channelId,
+      })
+      .getMany();
+    console.log('ret:', res);
+    return res;
+  }
 
-	async update(id: number, data: any) {
-		await this.partecipantDB
-		.createQueryBuilder()
-		.update(Partecipant)
-		.set({...data})
-		.where("partecipant.id = :uId", {uId: id})
-		.execute()
-	  }
-	}
-  
+  async isPartecipant(channelId: number, userId: number): Promise<boolean> {
+    const ret = await this.partecipantDB.findOne({
+      where: { channelId, userId },
+    });
+    return ret !== undefined;
+  }
+
+  async getPartecipantByUserAndChan(
+    userId: number,
+    channelId: number,
+  ): Promise<Partecipant> {
+    const ret = await this.partecipantDB.findOne({
+      where: { channelId, userId },
+    });
+    return ret;
+  }
+
+  async create(partecipant: Partecipant): Promise<Partecipant> {
+    return this.partecipantDB.save({
+      userId: partecipant.userId,
+      channelId: partecipant.channelId,
+      muted: partecipant.muted,
+      mod: partecipant.mod,
+    });
+  }
+
+  async delete(id: number) {
+    return this.partecipantDB.delete({ id });
+  }
+
+  async deleteAllByChan(chanId: number) {
+    return await this.partecipantDB
+      .createQueryBuilder()
+      .delete()
+      .from(Partecipant)
+      .where('channelId = :chId', { chId: chanId })
+      .execute();
+  }
+
+  async update(id: number, data: any) {
+    await this.partecipantDB
+      .createQueryBuilder()
+      .update(Partecipant)
+      .set({ ...data })
+      .where('partecipant.id = :uId', { uId: id })
+      .execute();
+  }
+}
